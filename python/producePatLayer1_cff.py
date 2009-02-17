e46fc99bd1e9ec3b12907e9ce2c9d18b427bf931
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-02-17 11:06:09 +0000

    added sequence for production of genMET incl. muons

--- python/producePatLayer1_cff.py
+++ python/producePatLayer1_cff.py
@@ -5,19 +5,26 @@ import FWCore.ParameterSet.Config as cms
 from TauAnalysis.RecoTools.electronPatProducer_cff import *
 from TauAnalysis.RecoTools.muonPatProducer_cff import *
 from TauAnalysis.RecoTools.pftauPatProducer_cff import *
+from TauAnalysis.RecoTools.pftauPatSelectorForDiTau_cfi import *
 from TauAnalysis.RecoTools.metPatProducer_cff import *
 from TauAnalysis.RecoTools.jetPatProducer_cff import *
 
 #import sequence for production of generator level tau-decay information
 from TauAnalysis.GenSimTools.tauGenJetProducer_cff import *
 
+#import sequence for production of generator level missing-Et
+# (with muons included)
+from TauAnalysis.GenSimTools.genMETWithMu_cff import *
+
 #import sequence for selection of primary event vertex candidates
 from TauAnalysis.RecoTools.eventVertexSelector_cfi import *
 
 producePatLayer1ForTauAnalyses = cms.Sequence( produceTauGenJetsForTauAnalyses
+                                              +produceGenMETwithMu
                                               +selectPrimaryVertexForTauAnalyses   
                                               +produceElectronsForTauAnalyses
                                               +produceMuonsForTauAnalyses          
                                               +producePFTausForTauAnalyses
+                                              +selectPFTausForDiTau # produce collection of tau-jet excluded from central jet veto
                                               +produceMissingEtForTauAnalyses
                                               +produceJetsForTauAnalyses )
