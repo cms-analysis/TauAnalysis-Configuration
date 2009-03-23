4dc9c5980f9b32d74a3e20e49481188027d6e299
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-03-23 07:37:36 +0000

    added first version of factorization of electron isolation efficiencies for Z --> e + tai-jet channel

--- python/factorizationTools.py
+++ python/factorizationTools.py
@@ -152,3 +152,149 @@ def enableFactorization_makeZtoMuTauPlots(process):
     process.plotZtoMuTau.processes.InclusivePPmuX.dqmDirectory = cms.string('InclusivePPmuX_factorized')
     process.plotZtoMuTau.processes.PPmuXptGt20.dqmDirectory = cms.string('PPmuXptGt20_factorized')
     process.plotZtoMuTau.processes.qcdSum.dqmDirectory = cms.string('qcdSum_factorized')
+
+#--------------------------------------------------------------------------------
+# utility functions specific to factorization
+# of muon isolation efficiencies in Z --> e + mu channel
+#--------------------------------------------------------------------------------
+
+def enableFactorization_runZtoElecMu(process):
+    process.load("TauAnalysis.Configuration.analyzeZtoElecMu_factorized_cff")
+    process.analyzeZtoElecMu_factorized = cms.Sequence( process.analyzeZtoElecMu_factorizedWithoutElectronIsolation
+                                                       *process.analyzeZtoElecMu_factorizedWithElectronIsolation )
+    process.p.replace(process.analyzeZtoElecMu, process.analyzeZtoElecMu_factorized)
+
+def makeZtoElecMuPlots_b1(inputDirectory,outputDirectory):
+    analyzer_b1 = cms.EDAnalyzer("DQMHistScaler",
+      dqmDirectory_input = inputDirectory,
+      dqmSubDirectories_input = cms.vstring( 'afterPrimaryEventVertex_beforePrimaryEventVertexQuality',
+                                             'afterPrimaryEventVertexQuality_beforePrimaryEventVertexPosition',
+                                             'afterPrimaryEventVertexPosition_beforeTightElectronIdCut',
+                                             'afterTightElectronIdCut_beforeElectronAntiCrackCut',
+                                             'afterElectronAntiCrackCut_beforeElectronEtaCut',
+                                             'afterElectronEtaCut_beforeElectronPtCut',
+                                             'afterElectronPtCut_beforeElectronTrkIsoCut',
+                                             'afterElectronTrkIsoCut_beforeElectronEcalIsoCut',
+                                             'afterElectronEcalIsoCut_beforeElectronTrkCut' ),
+      scaleFactor = cms.double(1.),
+      dqmDirectory_output = outputDirectory
+    )
+    return analyzer_b1
+def makeZtoElecMuPlots_b2(inputDirectory,outputDirectory):
+    analyzer_b2 = cms.EDAnalyzer("DQMHistScaler",
+      dqmDirectory_input = copy.deepcopy(inputDirectory),
+      dqmSubDirectories_input = cms.vstring( 'Trigger'
+                                             'primaryEventVertex',
+                                             'primaryEventVertexQuality',
+                                             'primaryEventVertexPosition',
+                                             'tightElectronIdCut',
+                                             'electronAntiCrackCut',
+                                             'electronEtaCut',
+                                             'electronPtCut',
+                                             'electronTrkIsoCut',
+                                             'electronEcalIsoCut' ),
+      scaleFactor = cms.double(1.),
+      dqmDirectory_output = copy.deepcopy(outputDirectory)
+    )
+    return analyzer_b2
+def makeZtoElecMuPlots_a1(inputDirectory,outputDirectory,directoryLooseSel,directoryTightSel):    
+    analyzer_a1 = cms.EDAnalyzer("DQMHistScaler",
+      dqmDirectory_input = copy.deepcopy(inputDirectory),
+      dqmSubDirectories_input = cms.vstring( 'afterElectronTrkCut_beforeElectronTrkIPcut',
+                                             'afterElectronTrkIPcut_beforeGlobalMuonCut',
+                                             'afterGlobalMuonCut_beforeMuonEtaCut',
+                                             'afterMuonEtaCut_beforeMuonPtCut',
+                                             'afterMuonPtCut_beforeMuonTrkIsoCut',
+                                             'afterMuonTrkIsoCut_beforeMuonEcalIsoCut',
+                                             'afterMuonEcalIsoCut_beforeMuonAntiPionCut',
+                                             'afterMuonAntiPionCut_beforeMuonTrkIPcut',
+                                             'afterMuonTrkIPcut_beforeDiTauCandidateForElecMuAcoplanarityCut',
+                                             'afterDiTauCandidateForElecMuAcoplanarityCut_beforeDiTauCandidateForElecMuZeroChargeCut' ),
+      dqmDirectory_factorizedLooseSel = copy.deepcopy(directoryLooseSel),
+      dqmDirectory_factorizedTightSel = copy.deepcopy(directoryTightSel),
+      meNameNumerator = cms.string('electronEcalIsoCut/passed_cumulative_numWeighted'),
+      meNameDenominator = cms.string('electronTrkIsoCut/processed_cumulative_numWeighted'),
+      meType = cms.string("real"),
+      dqmDirectory_output = copy.deepcopy(outputDirectory)
+    )
+    return analyzer_a1
+def makeZtoElecMuPlots_a2(inputDirectory,outputDirectory,directoryLooseSel,directoryTightSel):    
+    analyzer_a2 = cms.EDAnalyzer("DQMHistScaler",    
+      dqmDirectory_input = copy.deepcopy(inputDirectory),
+      dqmSubDirectories_input = cms.vstring( 'electronTrkCut',
+                                             'electronTrkIPcut',
+                                             'globalMuonCut',
+                                             'muonEtaCut',
+                                             'muonPtCut',
+                                             'muonTrkIsoCut',
+                                             'muonEcalIsoCut',
+                                             'muonAntiPionCut',
+                                             'muonTrkIPcut',
+                                             'diTauCandidateForElecMuAcoplanarityCut',
+                                             'diTauCandidateForElecMuZeroChargeCut' ),
+      dqmDirectory_factorizedLooseSel = copy.deepcopy(directoryLooseSel),
+      dqmDirectory_factorizedTightSel = copy.deepcopy(directoryTightSel),
+      meNameNumerator = cms.string('electronEcalIsoCut/passed_cumulative_numWeighted'),
+      meNameDenominator = cms.string('electronTrkIsoCut/processed_cumulative_numWeighted'),
+      meType = cms.string("real"),
+      dqmDirectory_output = copy.deepcopy(outputDirectory)
+    )
+    return analyzer_a2
+def enableFactorization_makeZtoElecMuPlots(process):
+    inputDirectory_InclusivePPmuXb1 = cms.string('InclusivePPmuX/zElecMuAnalyzer_factorizedWithElectronIsolation/')
+    outputDirectory_InclusivePPmuXb1 = cms.string('InclusivePPmuX_factorized/zElecMuAnalyzer/')
+    process.scaleZtoElecMu_InclusivePPmuXb1 = makeZtoElecMuPlots_b1(inputDirectory_InclusivePPmuXb1,outputDirectory_InclusivePPmuXb1)
+    inputDirectory_InclusivePPmuXb2 = cms.string('InclusivePPmuX/zElecMuAnalyzer_factorizedWithElectronIsolation/FilterStatistics/')
+    outputDirectory_InclusivePPmuXb2 = cms.string('InclusivePPmuX_factorized/zElecMuAnalyzer/FilterStatistics/')
+    process.scaleZtoElecMu_InclusivePPmuXb2 = makeZtoElecMuPlots_b2(inputDirectory_InclusivePPmuXb2,outputDirectory_InclusivePPmuXb2)
+    inputDirectory_InclusivePPmuXa1 = cms.string('InclusivePPmuX/zElecMuAnalyzer_factorizedWithoutElectronIsolation/')
+    outputDirectory_InclusivePPmuXa1 = cms.string('InclusivePPmuX_factorized/zElecMuAnalyzer/')
+    directoryLooseSel_InclusivePPmuXa1 = cms.string('InclusivePPmuX/zElecMuAnalyzer_factorizedWithoutElectronIsolation/FilterStatistics/')
+    directoryTightSel_InclusivePPmuXa1 = cms.string('InclusivePPmuX/zElecMuAnalyzer_factorizedWithElectronIsolation/FilterStatistics/')
+    process.scaleZtoElecMu_InclusivePPmuXa1 = makeZtoElecMuPlots_a1(inputDirectory_InclusivePPmuXa1,outputDirectory_InclusivePPmuXa1,
+                                                                    directoryLooseSel_InclusivePPmuXa1,directoryTightSel_InclusivePPmuXa1)
+    inputDirectory_InclusivePPmuXa2 = cms.string('InclusivePPmuX/zElecMuAnalyzer_factorizedWithoutElectronIsolation/FilterStatistics/')
+    outputDirectory_InclusivePPmuXa2 = cms.string('InclusivePPmuX_factorized/zElecMuAnalyzer/FilterStatistics/')
+    directoryLooseSel_InclusivePPmuXa2 = directoryLooseSel_InclusivePPmuXa1
+    directoryTightSel_InclusivePPmuXa2 = directoryTightSel_InclusivePPmuXa1
+    process.scaleZtoElecMu_InclusivePPmuXa2 = makeZtoElecMuPlots_a2(inputDirectory_InclusivePPmuXa2,outputDirectory_InclusivePPmuXa2,
+                                                                    directoryLooseSel_InclusivePPmuXa2,directoryTightSel_InclusivePPmuXa2)
+    
+    inputDirectory_PPmuXptGt20b1 = cms.string('PPmuXptGt20/zElecMuAnalyzer_factorizedWithElectronIsolation/')
+    outputDirectory_PPmuXptGt20b1 = cms.string('PPmuXptGt20_factorized/zElecMuAnalyzer/')
+    process.scaleZtoElecMu_PPmuXptGt20b1 = makeZtoElecMuPlots_b1(inputDirectory_PPmuXptGt20b1,outputDirectory_PPmuXptGt20b1)
+    inputDirectory_PPmuXptGt20b2 = cms.string('PPmuXptGt20/zElecMuAnalyzer_factorizedWithElectronIsolation/FilterStatistics/')
+    outputDirectory_PPmuXptGt20b2 = cms.string('PPmuXptGt20_factorized/zElecMuAnalyzer/FilterStatistics/')
+    process.scaleZtoElecMu_PPmuXptGt20b2 = makeZtoElecMuPlots_b2(inputDirectory_PPmuXptGt20b2,outputDirectory_PPmuXptGt20b2)
+    inputDirectory_PPmuXptGt20a1 = cms.string('PPmuXptGt20/zElecMuAnalyzer_factorizedWithoutElectronIsolation/')
+    outputDirectory_PPmuXptGt20a1 = cms.string('PPmuXptGt20_factorized/zElecMuAnalyzer/')
+    directoryLooseSel_PPmuXptGt20a1 = cms.string('PPmuXptGt20/zElecMuAnalyzer_factorizedWithoutElectronIsolation/FilterStatistics/')
+    directoryTightSel_PPmuXptGt20a1 = cms.string('PPmuXptGt20/zElecMuAnalyzer_factorizedWithElectronIsolation/FilterStatistics/')
+    process.scaleZtoElecMu_PPmuXptGt20a1 = makeZtoElecMuPlots_a1(inputDirectory_PPmuXptGt20a1,outputDirectory_PPmuXptGt20a1,
+                                                                 directoryLooseSel_PPmuXptGt20a1,directoryTightSel_PPmuXptGt20a1)
+    inputDirectory_PPmuXptGt20a2 = cms.string('PPmuXptGt20/zElecMuAnalyzer_factorizedWithoutElectronIsolation/FilterStatistics/')
+    outputDirectory_PPmuXptGt20a2 = cms.string('PPmuXptGt20_factorized/zElecMuAnalyzer/FilterStatistics/')
+    directoryLooseSel_PPmuXptGt20a2 = directoryLooseSel_PPmuXptGt20a1
+    directoryTightSel_PPmuXptGt20a2 = directoryTightSel_PPmuXptGt20a1
+    process.scaleZtoElecMu_PPmuXptGt20a2 = makeZtoElecMuPlots_a2(inputDirectory_PPmuXptGt20a2,outputDirectory_PPmuXptGt20a2,
+                                                                 directoryLooseSel_PPmuXptGt20a2,directoryTightSel_PPmuXptGt20a2)
+
+    process.addZtoElecMu_qcdSum.qcdSum.dqmDirectories_input = cms.vstring('InclusivePPmuX_factorized',
+                                                                          'PPmuXptGt20_factorized')
+    process.addZtoElecMu_qcdSum.qcdSum.dqmDirectory_output = cms.string('qcdSum_factorized')
+
+    process.addZtoElecMu_smSum.smSum.dqmDirectories_input = cms.vstring('Ztautau',
+                                                                        'Zmumu',
+                                                                        'WplusJets',
+                                                                        'qcdSum_factorized')
+    process.addZtoElecMu_smSum.smSum.dqmDirectory_output = cms.string('smSum_factorized')
+
+    process.addZtoElecMu = cms.Sequence( process.scaleZtoElecMu_InclusivePPmuXb1 + process.scaleZtoElecMu_InclusivePPmuXb2
+                                        +process.scaleZtoElecMu_InclusivePPmuXa1 + process.scaleZtoElecMu_InclusivePPmuXa2
+                                        +process.scaleZtoElecMu_PPmuXptGt20b1 + process.scaleZtoElecMu_PPmuXptGt20b2
+                                        +process.scaleZtoElecMu_PPmuXptGt20a1 + process.scaleZtoElecMu_PPmuXptGt20a2 
+                                        +process.addZtoElecMu_qcdSum + process.addZtoElecMu_smSum )
+    
+    process.plotZtoElecMu.processes.InclusivePPmuX.dqmDirectory = cms.string('InclusivePPmuX_factorized')
+    process.plotZtoElecMu.processes.PPmuXptGt20.dqmDirectory = cms.string('PPmuXptGt20_factorized')
+    process.plotZtoElecMu.processes.qcdSum.dqmDirectory = cms.string('qcdSum_factorized')
