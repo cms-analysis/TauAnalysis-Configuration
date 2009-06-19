fa995a831dac673be25992356066c930de9fd453
Monica Vazquez Acosta <sha1-d6d4e0778bbf59996987404c0bdf8ea90d29fdba@cern.ch>
2009-06-19 12:35:07 +0000

    commit tools to replace summary table titles

--- python/analysisSequenceTools.py
+++ python/analysisSequenceTools.py
@@ -38,3 +38,26 @@ def replaceHistManagerInputTags(analysisSequence, replacements):
 
                     
 
+def replaceTitles(analysisSequence, replacements):
+    # check that replacement is a list of type [ replace_1, replace_2 .. replace_N ]
+    # with each replace_i being a list with exactly two entries
+    # (first entry = string to be replace, second entry = substitute string)
+    for replacement in replacements:
+        #print (replacements)
+        assert isinstance(replacement,list) and len(replacement) == 2, \
+               "Error in <replaceTitles>: Invalid argument replacements = " + replacements
+
+    # substitute in all "title" attributes of analysisSequence object given as function argument
+    # all occurences of replace_i[first entry] by replace_i[second entry] (for all replace_i)
+    for pset in analysisSequence:
+        if hasattr(pset, "title"):
+            entries_orig = getattr(pset, "title")
+            #print(entries_orig)
+
+            for replacement in replacements:
+                replacement_orig = replacement[0]
+                replacement_mod = replacement[1]    
+
+                if str(entries_orig) == str(replacement_orig):
+                   setattr(pset,'title',replacement_mod)
+            #print getattr(pset,'title')
