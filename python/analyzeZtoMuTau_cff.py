44214c20c01da67045e88ed7da6916aa8ccdcb54
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-07 16:26:56 +0000

    version 1.29 back-ported from CMSSW_3_1_x branch with SysUncertaintyService removed

--- python/analyzeZtoMuTau_cff.py
+++ python/analyzeZtoMuTau_cff.py
@@ -3,17 +3,15 @@ import FWCore.ParameterSet.Config as cms
 # import config for event selection, event print-out and analysis sequence
 from TauAnalysis.Configuration.analyzeZtoMuTau_cfi import *
 
+# import definitions of systematic uncertainties
+from TauAnalysis.CandidateTools.sysErrDefinitions_cfi import *
+
 analyzeZtoMuTauEvents = cms.EDAnalyzer("GenericAnalyzer",
   
     name = cms.string('zMuTauAnalyzer'), 
                             
     filters = cms.VPSet(
         # generator level phase-space selection
-        # (NOTE: (1) to be used in case of Monte Carlo samples
-        #            overlapping in simulated phase-space only !!
-        #        (2) genPhaseSpaceCut needs to be **always** the first entry in the list of cuts
-        #           - otherwise the script submitToBatch.csh for submission of cmsRun jobs
-        #            to the CERN batch system will not work !!)
         genPhaseSpaceCut,
     
         # generator level selection of Z --> mu + tau-jet events
@@ -56,25 +54,43 @@ analyzeZtoMuTauEvents = cms.EDAnalyzer("GenericAnalyzer",
         evtSelDiTauCandidateForMuTauZeroCharge,
         evtSelDiTauCandidateForMuTauAcoplanarity12,
         evtSelDiTauCandidateForMuTauMt1MET,
-        evtSelDiTauCandidateForMuTauPzetaDiff
+        evtSelDiTauCandidateForMuTauPzetaDiff,
+
+        # Z --> mu+ mu- hypothesis veto (based on combinations of muon pairs)
+        evtSelDiMuPairZmumuHypothesisVeto
     ),
   
     analyzers = cms.VPSet(
         genPhaseSpaceEventInfoHistManager,
+        eventWeightHistManager,
         muonHistManager,
         tauHistManager,
         diTauCandidateHistManagerForMuTau,
+        diTauCandidateCollinearApproxHistManagerForMuTau,
+        diTauCandidateCollinearApproxHistManagerBinnedForMuTau,
+        diTauCandidateCollinearApproxBinningForMuTau,
         diTauCandidateZmumuHypothesisHistManagerForMuTau,
+        muPairHistManager,
         jetHistManager,
-        metHistManager,
+        caloMEtHistManager,
+        pfMEtHistManager,
+        particleMultiplicityHistManager,
         vertexHistManager,
-        triggerHistManager,
-        eventWeightHistManager
+        triggerHistManagerForMuTau,
+        sysUncertaintyBinnerForMuTau
     ),
 
     eventDumps = cms.VPSet(
         muTauEventDump
     ),
    
-    analysisSequence = muTauAnalysisSequence
+    analysisSequence = muTauAnalysisSequence,
+
+    systematics = cms.vstring(
+        getSysUncertaintyNames(
+            [ muonSystematics,
+              tauSystematics,
+              theorySystematics ]
+        )
+    )
 )
