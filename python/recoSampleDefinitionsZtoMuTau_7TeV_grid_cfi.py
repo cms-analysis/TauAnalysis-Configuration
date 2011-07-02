846dee211dd1b2ac1225fe4aa765d15365435891
cmsbuild <>
2011-07-02 14:56:26 +0000

    This commit was manufactured by cvs2git to create tag 'TauAnalysis- Configuration-b4_2_x_2011Jul02'.
    Sprout from master 2011-06-28 15:42:26 UTC Jeff Kolb <sha1-db54915842bc6be6c5255beebf3371b954cb71fa@cern.ch> 'added 2011 PR_v4 e+tau skim'
    Cherrypick from master 2011-07-02 14:56:25 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'added recoSampleDefinitions file specific to TauIdEff measurement':
        python/recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi.py

--- python/recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi.py
+++ python/recoSampleDefinitionsZtoMuTau_7TeV_grid_cfi.py
@@ -7,7 +7,7 @@ import TauAnalysis.DQMTools.plotterStyleDefinitions_cfi as styles
 # List of samples to run in the analysis
 SAMPLES_TO_ANALYZE = [
     'data_SingleMu_Run2011A_May10ReReco_v1ex',
-    'data_TauPlusX_Run2011A_May10ReReco_v1',
+    #'data_TauPlusX_Run2011A_May10ReReco_v1',
     'data_TauPlusX_Run2011A_PromptReco_v4',
     #'DYtautauM10to20_powheg',
     'Ztautau_pythia',
@@ -105,27 +105,6 @@ RECO_SAMPLES = {
         'enableFakeRates' : True,
         'hlt' : cms.InputTag("TriggerResults", "", "HLT")
     },
-    'data_SingleMu_Run2011A_May10ReReco_v1' : {
-        'datasetpath' : '/SingleMu/Run2011A-May10ReReco-v1/AOD',
-        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
-        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON.txt",
-        'runselection' : "160329-163869",
-        'number_of_jobs' : 500,
-        'conditions' : 'GR_R_42_V14::All',
-        'events_processed' : -1,
-        'skim_eff' : 1.0,
-        'type' : 'Data',
-        'drawOption' : styles.drawOption_Data,
-        'hlt_paths' : {
-            'HLT_IsoMu12_v1' : '160431:MIN-163261:MAX',
-            'HLT_IsoMu17_v6' : '163270:MIN-163869:MAX',
-            'HLT_Mu24_v1'    : '160431:MIN-163261:MAX',
-            'HLT_Mu24_v2'    : '163270:MIN-163869:MAX',
-        },
-        'enableSysUncertainties' : False,
-        'enableFakeRates' : True,
-        'hlt' : cms.InputTag("TriggerResults", "", "HLT")
-    },
     'data_SingleMu_Run2011A_May10ReReco_v1ex' : {
         'datasetpath' : '/SingleMu/Run2011A-May10ReReco-v1/AOD',
         'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
@@ -148,8 +127,8 @@ RECO_SAMPLES = {
     'data_TauPlusX_Run2011A_PromptReco_v4' : {
         'datasetpath' : "/TauPlusX/Run2011A-PromptReco-v4/AOD",
         'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
-        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-167151_7TeV_PromptReco_Collisions11_JSON.txt",
-        'runselection' : "165071-167496",
+        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-167784_7TeV_PromptReco_Collisions11_JSON.txt",
+        'runselection' : "165071-167784",
         'number_of_jobs' : 1000,
         'conditions' : 'GR_R_42_V14::All',
         'events_processed' : -1,
@@ -159,27 +138,7 @@ RECO_SAMPLES = {
         'hlt_paths' : {
             'HLT_IsoMu15_LooseIsoPFTau15_v2' : '165088:MIN-165633:MAX',  # period D
             'HLT_IsoMu15_LooseIsoPFTau20_v2' : '163269:MIN-163869:MAX',  # period E
-            'HLT_IsoMu15_LooseIsoPFTau20_v4' : '167078:MIN-167151:MAX'   # period F
-        },
-        'enableSysUncertainties' : False,
-        'enableFakeRates' : True,
-        'hlt' : cms.InputTag("TriggerResults", "", "HLT")
-    },
-    'data_SingleMu_Run2011A_PromptReco_v4' : {
-        'datasetpath' : "/SingleMu/Run2011A-PromptReco-v4/AOD",
-        'dbs_url' :  "http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet",
-        'lumi_mask' : "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-167151_7TeV_PromptReco_Collisions11_JSON.txt",
-        'runselection' : "165071-167496",
-        'number_of_jobs' : 1000,
-        'conditions' : 'GR_R_42_V14::All',
-        'events_processed' : -1,
-        'skim_eff' : 1.0,
-        'type' : 'Data',
-        'drawOption' : styles.drawOption_Data,
-        'hlt_paths' : {
-            'HLT_IsoMu17_v8'  : '165088:MIN-165633:MAX',
-            'HLT_IsoMu17_v9'  : '165970:MIN-167043:MAX',
-            'HLT_IsoMu17_v11' : '167078:MIN-167151:MAX'
+            'HLT_IsoMu15_LooseIsoPFTau20_v4' : '167078:MIN-167784:MAX'   # period F
         },
         'enableSysUncertainties' : False,
         'enableFakeRates' : True,
